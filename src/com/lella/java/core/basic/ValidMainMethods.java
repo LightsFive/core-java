ac1f9077216e10bebc74cb6feaeb1021c9252fdb
NikhilLella <nikhileswarlella@gmail.com>
2024-06-06 18:30:02 +0530

    lella init

---
+++ src/com/lella/java/core/basic/ValidMainMethods.java
@@ -0,0 +1,25 @@
+package com.lella.java.core.basic;
+
+public class ValidMainMethods {
+ /*   public static void main(String...args){
+
+    }
+   public static void main (String[] args){
+
+    }
+   static public void main(String args[]){
+
+    }
+  public static final void main(String []args){
+        //static method declared final suggestion to remove
+    }
+    final public static void main(String...args){}
+
+    //reorder final suggestion
+}
+*/
+strictfp  public static void main(String...args){
+    System.out.println("Strictfp is redundant on java 17 and later");
+}
+}
+
